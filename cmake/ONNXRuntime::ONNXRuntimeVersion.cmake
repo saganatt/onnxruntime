1532a4a88ddd02883ea2cd3e979c2dd814e92119
Maja Kabus <mkabus@cern.ch>
2023-03-21 16:55:16 +0100

    Update package version

--- cmake/ONNXRuntime::ONNXRuntimeVersion.cmake
+++ cmake/ONNXRuntime::ONNXRuntimeVersion.cmake
@@ -1,7 +1,7 @@
 # Custom cmake version file by jcarius
 # From: https://stackoverflow.com/a/66494534
 
-set(PACKAGE_VERSION "1.12.1")
+set(PACKAGE_VERSION "1.14.1")
 
 # Check whether the requested PACKAGE_FIND_VERSION is compatible
 if("${PACKAGE_VERSION}" VERSION_LESS "${PACKAGE_FIND_VERSION}")
